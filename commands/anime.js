30ebd1bf549c15f4ef3bd572707911170a9de909
Pongsapat3791 <64452850+Pongsapat3791@users.noreply.github.com>
2020-07-31 21:23:12 +0700

    Update fix

--- commands/anime.js
+++ commands/anime.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'neko',
-	description: 'Image Cat!!!',
+	name: 'anime',
+	description: 'Random Image!!',
 	execute(message) {
 
         number = 114;
         imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-        message.channel.send ( {files: [ "./neko/" + imageNumber + ".png"]} )
+        message.channel.send ( {files: [ "./anime/" + imageNumber + ".png"]} )
 
     },
       
